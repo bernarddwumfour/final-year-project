2af80dea4423c6d01f718266c66b326af6cb1d8b
bernarddwumfour <bernarddwumfour25@gmail.com>
2024-08-16 00:06:35 +0000

    Added download option

--- app/components/modals/Policymodal.tsx
+++ app/components/modals/Policymodal.tsx
@@ -3,17 +3,30 @@ import Image from "next/image";
 import React, { useContext, useEffect, useState } from "react";
 import Template1 from "./policytemplates/Template1";
 import Template2 from "./policytemplates/Template2";
+import { usePDF } from 'react-to-pdf';
+
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 2) + 1;
 }
 const Policymodal = () => {
   const [random,setrandom] = useState<number>()
+  const { toPDF, targetRef } = usePDF({filename: 'Summarised privacy policy.pdf'});
 
   useEffect(()=>{
     setrandom(getRandomNumber())
   },[])
   const { togglemodalcontent, togglemodal } = useContext(Appcontext);
+  const [loading,setloading] =  useState(false)
+
+
+  const saveaspdf = ()=>{
+    setloading(true)
+    toPDF()
+    setTimeout(() => {
+      setloading(false)
+    },2000);
+  }
 
 
 
@@ -24,10 +37,14 @@ const Policymodal = () => {
 
   return (
     <div className="policy">
+      <div  ref={targetRef}>
       {random ==1 && <Template1 />}
       {random ==2 && <Template2 />}
+      </div>
       <div className="actions">
-        <button className="click">Save</button>
+        <button  className={`click ${loading && "inactive"}`} disabled = {loading}  onClick={() => saveaspdf()}>
+          {loading ? "Downloading" :"Save"}
+          </button>
         <button className="click secondary" onClick={cancel}>
           Cancel
         </button>
